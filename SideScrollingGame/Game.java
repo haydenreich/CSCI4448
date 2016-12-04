d8170b8f4446b0ad68a2aa670b852c6513195b67
Hayden Reich <hayden.reich@colorado.edu>
2016-12-04 13:08:15 -0700

    Modified Game and Menu
    Added basic start game menu and dummy help menu

---
+++ SideScrollingGame/Game.java
@@ -0,0 +1,35 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+	public Game() {
+		initUI();
+	}
+	
+	private Menu menu;
+	
+	void initUI() {
+		add(new Screen());
+		menu = new Menu();
+		setSize(600,600);
+		setTitle("Survive");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+	}
+	
+	public static void main(String[] args){
+		EventQueue.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				Game ex = new Game();
+				ex.setVisible(true);
+				ex.menu.setLocationRelativeTo(ex);
+				ex.menu.setVisible(true);
+				
+				
+			}
+		});
+	}
+}
